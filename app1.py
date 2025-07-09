60a4cd6144f97b1c4b91f9a93e441474addc2caf
Saed <danielsaed99@hotmail.com>
2025-07-08 21:03:45 -0600

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 # Reemplaza con tu email y contraseña de iRacing
 username = 'danielsaed99@hotmail.com'
-password = 'F3ChecoPerez11'
+password = ''
 idc = irDataClient(username=username, password=password)
 
 # ------------------------------------
-a = idc.driver_list(2)
+a = idc.driver_list(1)
 
 df = pd.DataFrame(a)
 
